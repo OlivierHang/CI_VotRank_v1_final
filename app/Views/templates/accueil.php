5712179cf31972a366e45a8e099006da52cb61cf
Olivier Hang <hang.sengolivier@gmail.com>
2021-01-26 00:01:23 +0100

    Message accueil DONE.

--- app/Views/templates/accueil.php
+++ app/Views/templates/accueil.php
@@ -1,22 +1,18 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.3/tiny-slider.css">
-<div id="conteneurSlider" style="text-align: center;">
-    <div class="my-slider">
-        <div class="slide-item"><img src="https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI" alt=""></div>
-        <div class="slide-item"><img src="https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI" alt=""></div>
-        <div class="slide-item"><img src="https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI" alt=""></div>
+<div id="conteneurPrincipalAccueil">
+    <div id="conteneurTextParent">
+        <div id="conteneurTextAccueil">
+            <p>
+                <span id="titre">Bienvenue sur Vot'Rank</span><br>
+
+                Ici vous pourrez créer des sondages et voter avec un système de classement.<br>
+                Que ce soit des sondages entre amis ou d'autre utilisation, <br>
+                Vot'Rank s'adresse à tout le monde !!
+
+            </p>
+        </div>
+    </div>
+    <div id="conteneurBtn">
+        <a id="btnStart" class="btn btn-primary" href="<?= base_url('public/vote') ?>" role="button">Let's make a Vot'Rank !</a>
     </div>
 </div>
-<div id="conteneur">
-    <a id="btnStart" class="btn btn-primary" href="<?= base_url('public/vote') ?>" role="button">Let's make a Vot'Rank !</a>
-</div>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js"></script>
-<script>
-    let slider = tns({
-        "container": ".my-slider",
-        "autoHeight": true,
-        "items": 1,
-        "swipeAngle": false,
-        "speed": 400
-    });
-</script>
